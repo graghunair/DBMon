0acf1d98edb2c14616669ea046c5ec8ae8fb669b
Raghu Gopalakrishnan <graghunair@gmail.com>
2022-01-25 22:58:17 +0300

    Update uspDBMon_Track_SQLServer_Instance_Restarts.sql

--- Track Uptime/uspDBMon_Track_SQLServer_Instance_Restarts.sql
+++ Track Uptime/uspDBMon_Track_SQLServer_Instance_Restarts.sql
@@ -1,12 +1,14 @@
 SET NOCOUNT ON
 GO
 
+--Change the database name if necessary
 USE [dba_local]
 GO
 
-DROP PROC IF EXISTS [dbo].[uspDBMon_Track_SQLServer_Instance_Restarts]
+--Drop the procedure if exists to accomodate the changes.
+DROP PROC IF EXISTS [dbo].[uspDBMon_Track_SQLServer_Instance_Restart]
 GO
-CREATE PROC [dbo].[uspDBMon_Track_SQLServer_Instance_Restarts]
+CREATE PROC [dbo].[uspDBMon_Track_SQLServer_Instance_Restart]
 AS
 SET NOCOUNT ON
 /*
@@ -14,30 +16,32 @@ SET NOCOUNT ON
 	Purpose		:		Track SQL Server instance restarts and record it in the table:
 	Version		:		1.0
 	Execute		:
-						EXEC [dbo].[uspDBMon_Track_SQLServer_Instance_Restarts]
+						EXEC [dbo].[uspDBMon_Track_SQLServer_Instance_Restart]
 
 						SELECT	TOP 10 *, DATEDIFF(mi, [SQLServer_Start_Time], [Last_Updated]) AS UpTime_In_Minutes
-						FROM	[dbo].[tblDBMon_Track_Instance_Restarts]
+						FROM	[dbo].[tblDBMon_Track_Instance_Restart]
 						ORDER BY [ID] DESC
-	Modification:
-		
+	Modification:	
 			Jan 25th, 2022	:	Raghu		:	Inception
 */
 
 DECLARE @varOld_SQLServer_Start_Time DATETIME,
 		@varNew_SQLServer_Start_Time DATETIME 
 
+--Get the last time SQL Server instance restart was recorded
 SELECT	@varOld_SQLServer_Start_Time = [sqlserver_start_time]
-FROM	[dbo].[tblDBMon_Track_Instance_Restarts]
+FROM	[dbo].[tblDBMon_Track_Instance_Restart]
 WHERE	[Current] = 1
 
+--Get the currect SQL Server instance restart from DMV
 SELECT	@varNew_SQLServer_Start_Time = [sqlserver_start_time]
 FROM	[sys].[dm_os_sys_info]
 
+--Compare the 2 timestamp to identify whether an instance restart has occurred
 IF (@varOld_SQLServer_Start_Time = @varNew_SQLServer_Start_Time)
 	BEGIN
 		PRINT 'SQL Server Instance restart not detected.'
-		UPDATE	[dbo].[tblDBMon_Track_Instance_Restarts]
+		UPDATE	[dbo].[tblDBMon_Track_Instance_Restart]
 		SET		[Last_Updated] = GETDATE()
 		WHERE	[Current] = 1
 	END
@@ -45,14 +49,15 @@ ELSE
 	BEGIN
 		PRINT 'SQL Server Instance restart detected.'
 
-		UPDATE	[dbo].[tblDBMon_Track_Instance_Restarts] 
+		UPDATE	[dbo].[tblDBMon_Track_Instance_Restart] 
 		SET		[Current] = 0
 		WHERE	[Current] = 1
 
-		INSERT INTO [dbo].[tblDBMon_Track_Instance_Restarts] ([Last_Updated], [SQLServer_Start_Time])
-		SELECT GETDATE(), [SQLServer_Start_Time] FROM [sys].[dm_os_sys_info] 
+		INSERT INTO [dbo].[tblDBMon_Track_Instance_Restart] ([Last_Updated], [SQLServer_Start_Time], [Host])
+		SELECT	GETDATE(), [sqlserver_start_time], CAST(SERVERPROPERTY('ComputerNamePhysicalNetBIOS') AS NVARCHAR(128))
+		FROM	[sys].[dm_os_sys_info]
 	END
 GO
 
-EXEC	[dbo].[uspDBMon_Track_SQLServer_Instance_Restarts]
+EXEC	[dbo].[uspDBMon_Track_SQLServer_Instance_Restart]
 GO
