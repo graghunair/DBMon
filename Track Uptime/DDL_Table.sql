0a3aa85a13f448de645518acc8d0b0945b54723c
Raghu Gopalakrishnan <graghunair@gmail.com>
2022-01-25 22:55:41 +0300

    Update DDL_Table.sql

--- Track Uptime/DDL_Table.sql
+++ Track Uptime/DDL_Table.sql
@@ -1,24 +1,40 @@
 SET NOCOUNT ON
 GO
 
+--Change the database name if necessary
 USE [dba_local]
 GO
 
-DROP TABLE IF EXISTS [dbo].[tblDBMon_Track_Instance_Restarts]
+--Drop the table if already exists to accomodate any schema changes and start fresh
+DROP TABLE IF EXISTS [dbo].[tblDBMon_Track_Instance_Restart]
 GO
 
-CREATE TABLE [dbo].[tblDBMon_Track_Instance_Restarts](
-	[ID]			BIGINT IDENTITY(1,1),
-	[SQLServer_Start_Time] DATETIME,
-	[Last_Updated]	DATETIME,
-	[Current]		BIT DEFAULT 1)
-
-CREATE CLUSTERED INDEX IDX_tblDBMon_Track_Instance_Restarts ON [dbo].[tblDBMon_Track_Instance_Restarts]([ID])
+--Create the table to store historical information about SQL Server Instance restarts
+CREATE TABLE [dbo].[tblDBMon_Track_Instance_Restart](
+	[ID]					INT IDENTITY(1,1),
+	[SQLServer_Start_Time]	DATETIME,
+	[Last_Updated]			DATETIME,
+	[Host]					NVARCHAR(128),
+	[Current]				BIT)
 GO
 
-INSERT INTO [dbo].[tblDBMon_Track_Instance_Restarts] ([Last_Updated], [SQLServer_Start_Time], [Current])
-SELECT	GETDATE(), [sqlserver_start_time], 1 FROM [sys].[dm_os_sys_info] 
+--Created clustered index on the ID column to sort it
+CREATE CLUSTERED INDEX [IDX_tblDBMon_Track_Instance_Restart]
+ON [dbo].[tblDBMon_Track_Instance_Restart]([ID])
+GO
 
+--Add the default constraint to auto-populate 1 for the column Current
+ALTER TABLE [dbo].[tblDBMon_Track_Instance_Restart] 
+ADD CONSTRAINT [DF_tblDBMon_Track_Instance_Restart_Currect] 
+DEFAULT 1 FOR [Current]
+GO
+
+--Insert the first record into the table
+INSERT INTO [dbo].[tblDBMon_Track_Instance_Restart] ([Last_Updated], [SQLServer_Start_Time], [Host], [Current])
+SELECT	GETDATE(), [sqlserver_start_time], CAST(SERVERPROPERTY('ComputerNamePhysicalNetBIOS') AS NVARCHAR(128)), 1 
+FROM	[sys].[dm_os_sys_info] 
+
+--Select from the table
 SELECT	*, DATEDIFF(mi, [SQLServer_Start_Time], [Last_Updated]) AS UpTime_In_Minutes
-FROM	[dbo].[tblDBMon_Track_Instance_Restarts]
+FROM	[dbo].[tblDBMon_Track_Instance_Restart]
 GO
