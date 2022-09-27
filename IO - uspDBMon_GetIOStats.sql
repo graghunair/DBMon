f83b95864a8a0ac853a0faabe881ee751fa19f2c
Raghu Gopalakrishnan <graghunair@gmail.com>
2022-09-27 10:54:23 +0300

    Update IO - uspDBMon_GetIOStats.sql

--- IO - uspDBMon_GetIOStats.sql
+++ IO - uspDBMon_GetIOStats.sql
@@ -7,12 +7,13 @@
 				EXEC [dbo].[uspDBMon_GetWaitStats]
 				SELECT * FROM [dbo].[tblDBMon_DM_OS_Wait_Stats]
 
-				Reference: https://www.sqlskills.com/blogs/paul/wait-statistics-or-please-tell-me-where-it-hurts/
+				Reference: https://www.sqlskills.com/blogs/paul/how-to-examine-io-subsystem-latencies-from-within-sql-server/
 				
 	Modification History
 	-----------------------
 	Sept 27th, 2022	:	v1.0	:	Raghu	:	Inception
 */
+
 USE [dba_local]        
 GO        
 
@@ -21,19 +22,23 @@ PRINT 'Table: [dbo].[tblDBMon_DM_IO_Virtual_File_Stats] dropped.'
 GO         
 
 CREATE TABLE [dbo].[tblDBMon_DM_IO_Virtual_File_Stats](                            
-	[Date_Captured] [datetime] NOT NULL CONSTRAINT [DF_tblDBMon_DM_IO_Virtual_File_Stats_Date_Captured]  DEFAULT (getdate()),              
-	[Database_Name] [nvarchar](128) NULL,                            
-	[Type] [nvarchar](60) NULL,                            
-	[Logical_Name] [sysname] NOT NULL,                      
-	[Reads] [bigint] NOT NULL,                            
-	[Reads_KB] [bigint] NULL,                            
+	[Date_Captured]		[datetime] NOT NULL CONSTRAINT [DF_tblDBMon_DM_IO_Virtual_File_Stats_Date_Captured]  DEFAULT (getdate()),              
+	[Database_Name]		[nvarchar](128) NULL,                            
+	[Type]				[nvarchar](60) NULL,                            
+	[Logical_Name]		[sysname] NOT NULL,                      
+	[Reads]				[bigint] NOT NULL,                            
+	[Reads_MB]			[bigint] NULL,                            
 	[Reads_IO_Stalls_ms] [bigint] NOT NULL,                            
-	[Writes] [bigint] NOT NULL,                            
-	[Writes_KB] [bigint] NULL,                            
+	[Writes]			[bigint] NOT NULL,                            
+	[Writes_MB]			[bigint] NULL,                            
 	[Writes_IO_Stalls_ms] [bigint] NOT NULL,                            
 	[Total_IO_Stall_ms] [bigint] NOT NULL,                            
-	[File_Size_MB] [numeric](20, 2) NULL,                            
-	[PhysicalName] [nvarchar](260) NOT NULL)       
+	[File_Size_MB]		[bigint] NULL,                            
+	[Physical_Name]		[nvarchar](260) NOT NULL)       
+GO
+
+CREATE CLUSTERED INDEX [IDX_tblDBMon_DM_IO_Virtual_File_Stats_Date_Captured] 
+ON [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]([Date_Captured])
 GO
 
 DROP PROCEDURE IF EXISTS [dbo].[uspDBMon_GetIOStats]    
@@ -42,54 +47,53 @@ GO
 
 CREATE PROCEDURE [dbo].[uspDBMon_GetIOStats]
 AS
-
 /*    
 	Author	:    Raghu Gopalakrishnan    
-	Date	:    07 January 2020    
-	Purpose	:    This Stored Procedure is used to capture IO Stats.    
+	Date	:    27th Sept 2022
+	Purpose	:    This Stored Procedure is used to capture disk IO Stats.    
 	Version :    1.0                              
 
-			exec [dbo].[uspDBMon_GetIOStats]                
+			EXEC [dbo].[uspDBMon_GetIOStats]                
 			SELECT * FROM [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]      
 			
 	Modification History    
 	-----------------------    
-	Sept 15th, 2014    :    v1.0    :    Raghu Gopalakrishnan    :    Inception
+	Sept 27th, 2022    :    v1.0    :    Raghu Gopalakrishnan    :    Inception
 */
 SET NOCOUNT ON  
+
 --Capture the IO-stats
+INSERT INTO [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]
+SELECT		GETDATE()												AS [Date_Captured],
+			DB_NAME(v.[database_id])								AS [Database_Name],            
+			[type_desc]												AS [Type], 
+			[name]													AS [Logical_Name],            
+			[num_of_reads]											AS [Reads],  
+			CAST([num_of_bytes_read]/1024./1024. AS DECIMAL(20,0))	AS [Reads_MB], 
+			[io_stall_read_ms]										AS [Reads_IO_Stalls_ms],            
+			[num_of_writes]											AS [Writes], 
+			CAST([num_of_bytes_written]/1024./1024. AS DECIMAL(20,0)) AS [Writes_MB], 
+			[io_stall_write_ms]										AS [Writes_IO_Stalls_ms],            
+			[io_stall]												AS [Total_IO_Stall_ms],            
+			cast([size_on_disk_bytes]/1024./1024. AS DECIMAL(20,0))	AS [File_Size_MB],            
+			[physical_name]											AS [Physical_Name]
+FROM        sys.dm_io_virtual_file_stats(NULL, NULL) v
+INNER JOIN  sys.master_files f        
+	ON    	(v.[database_id] = f.[database_id] AND v.[file_id] = f.[file_id])
 
-INSERT INTO 	[dbo].[tblDBMon_DM_IO_Virtual_File_Stats]
-SELECT		GETDATE() AS Date_Captured,
-		DB_NAME(v.database_id) as 'Database_Name',            
-		type_desc as 'Type', 
-		name as 'Logical_Name',            
-		num_of_reads as 'Reads',  
-		num_of_bytes_read/1024 as 'Reads_KB', 
-		io_stall_read_ms as 'Reads_IO_Stalls_ms',            
-		num_of_writes as 'Writes', 
-		num_of_bytes_written/1024 as 'Writes_KB', 
-		io_stall_write_ms as 'Writes_IO_Stalls_ms',            
-		io_stall as 'Total_IO_Stall_ms',            
-		cast(size_on_disk_bytes/1048576.0 as numeric(20,2)) as 'File_Size_MB',            
-		physical_name as 'PhysicalName'
-FROM        	sys.dm_io_virtual_file_stats(NULL, NULL) v
-INNER JOIN    	sys.master_files f        
-	ON    	(v.database_id=f.database_id AND v.file_id=f.file_id)
-
-DELETE TOP (10000)FROM    [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]
-WHERE    [Date_Captured] < GETDATE() - 10
+DELETE	TOP (10000)
+FROM	[dbo].[tblDBMon_DM_IO_Virtual_File_Stats]
+WHERE	[Date_Captured] < GETDATE() - 100
 GO
 
-
-exec sp_addextendedproperty            
+EXEC sp_addextendedproperty            
 @name = 'Version', @value = '1.0',           
 @level0type = 'SCHEMA', 
 @level0name = 'dbo',            
 @level1type = 'PROCEDURE', 
 @level1name = 'uspDBMon_GetIOStats'    
-
+GO
 
 EXEC [dbo].[uspDBMon_GetIOStats]    
-
-SELECT * FROM [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]  
+SELECT * FROM [dbo].[tblDBMon_DM_IO_Virtual_File_Stats]
+GO
